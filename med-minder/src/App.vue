3919eb0f8a4f2e437b364a84e3a41485e4cf1cfb
Kristina Lokar <lokarkristina79@gmail.com>
2025-01-09 13:10:31 +0100

    Prepare initial setup.

---
+++ med-minder/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterView } from 'vue-router'
+// components
+import AppFooter from '@/components/AppFooter.vue'
+</script>
+
+<template>
+  <header>
+    <img src="@/assets/logo.png" alt="MedMinder logo" class="logo">
+
+    Medminder - Your medication supply guide.
+
+    <nav class="main-menu">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+
+  <AppFooter />
+
+</template>
+
+<style scoped>
+.logo {
+  max-width: 60px;
+}
+</style>
---
+++ med-minder/src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <footer>
+    <p>Created by Kristina Lokar</p>
+    <div class="socials">
+      <a href="https://www.linkedin.com/in/lokar-kristina">
+      </a>
+      <a href="https://github.com/lokarkristina"></a>
+    </div>
+  </footer>
+</template>
