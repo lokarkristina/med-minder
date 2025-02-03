57e121b8836239ecc5c69b0c130f55f8db0a55a9
Kristina Lokar <lokarkristina79@gmail.com>
2025-02-03 13:59:09 +0100

    MMINDER-10 Initial setup & add navigation.

---
+++ src/components/elements/Header.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from '@/components/ui/navigation-menu'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="200" />
+
+    <!-- this menu is so large. maybe it can be cleaned a bit? @todo -->
+     <!-- @todo shouldn't we be using RouterLink? -->
+    <NavigationMenu>
+      <NavigationMenuList>
+        <!-- medicine info -->
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Medication</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul
+              class="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[minmax(0,.75fr)_minmax(0,1fr)]"
+            >
+              <li class="row-span-3">
+                <NavigationMenuLink as-child>
+                  <a
+                    class="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    Meds overview
+                  </a>
+                </NavigationMenuLink>
+              </li>
+
+              <li>
+                <NavigationMenuLink as-child>
+                  <a
+                    href="/"
+                    class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                  >
+                    Add another med
+                  </a>
+                </NavigationMenuLink>
+              </li>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <!-- about section -->
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>More info</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul class="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+              <li>
+                <NavigationMenuLink as-child>
+                  <a
+                    :href="'/about'"
+                    class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                  >
+                    About
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <li>
+                <NavigationMenuLink as-child>
+                  <a
+                    :href="'/about'"
+                    class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                  >
+                    Documentation
+                  </a>
+                </NavigationMenuLink>
+              </li>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <!-- account menu -->
+        <NavigationMenuItem>
+          <NavigationMenuLink as-child> Account </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  </header>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>
+      The home page with a navigation.
+    </h1>
+    <Button>
+      The button!
+    </Button>
+  </main>
+</template>
